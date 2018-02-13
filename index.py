46506c0843fad88c44da3edd7f8f68cdb050cdd8
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-02-13 13:36:16 +0530

    get list of coins

---
+++ index.py
@@ -0,0 +1,9 @@
+import requests
+
+BASE_URL = 'https://shapeshift.io/%s'
+
+def get_coins():
+    url_path = 'getcoins'
+    url = BASE_URL % url_path
+    response = requests.get(url)
+    return response.json()
